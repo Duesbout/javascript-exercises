6be174ba82baa56436949ad2a81f4b1cce2b9267
Jacob Duesbout <JacobDuesbout@gmail.com>
2022-03-29 19:03:11 -0400

    I've done like 6 of these today and I'm taking a break

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const thisYear = new Date().getFullYear();
 
-};
+  return people.reduce((old, person) => {
+    const age = (person.yearOfDeath || thisYear) - person.yearOfBirth;
+    return age > old.age ? { person, age } : old;
+  }, { person: null, age: 0 }).person;
+}
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
