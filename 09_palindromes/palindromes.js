9e9f3fc5ad183d30556cbdbb81d9384bdbc96a46
Jacob Duesbout <JacobDuesbout@gmail.com>
2022-03-29 16:07:27 -0400

    A quick and clean solution for reversing palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
+const palindromes = function checkPalindrom (str) {
+    str = str.toLowerCase().replace(/\W/g, ''); //Changes the string to lowercase and removes any non-letters
+    return str == str.split('').reverse().join('');
+  }
+  
 
-};
 
 // Do not edit below this line
 module.exports = palindromes;
