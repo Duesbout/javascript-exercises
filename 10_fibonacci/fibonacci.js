93ba4f3f7f98a87c84337d6cfa3aab87fa70fc50
Jacob Duesbout <JacobDuesbout@gmail.com>
2022-03-29 17:29:50 -0400

    A clear and quick fibonacci sequence N checker

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function fibonacci(num) {
+    let a = 0, b = 1, temp;
+    if (num === 0) return 0;
+    if (num < 0) return "OOPS";
+ 
+  while (num >= 0) {
+      temp = a;
+      a = a + b;
+      b = temp;
+      num--;
+  }
+    
+    return b;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
