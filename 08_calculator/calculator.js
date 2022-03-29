b127eb15584f24ce074bdbc56e95e1e123609048
Jacob Duesbout <JacobDuesbout@gmail.com>
2022-03-29 02:17:34 -0400

    Added .reduce to the multiply constant and slimmed down the power constant.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -4,12 +4,11 @@ const subtract = (a,b) => a - b;
 
 const sum = [].reduce((a,b) => a + b, 0);
 	
+const multiply = [].reduce((a,b) => a * b, 0);
 
-const multiply = ([a,b]) => a * b;
+const power = () => Math.pow(a,b);
 
-const power = (a,b) = a ** b;
-
-const factorial = (x: number): number => (x === 0) ? 1 : (x * this.factorial(x-1));
+const factorial = (x: Number): Number => (x === 0) ? 1 : (x * this.factorial(x-1));
 	
 // Do not edit below this line
 module.exports = {
